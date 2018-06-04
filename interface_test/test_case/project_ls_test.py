d83c3a53fea4ee98644ff62c296d933b67087ebc
qwangzone <578380132@qq.com>
2018-06-04 10:21:03 +0800

    优化散标列表case

--- interface_test/test_case/project_ls_test.py
+++ interface_test/test_case/project_ls_test.py
@@ -17,12 +17,13 @@ class ProjectList(unittest.TestCase):
                            ('web_projectCategory_xin', 'WEB', '2', '1', '2', '', '', '','信易融','信易融'),
                            ('web_projectCategory_fang', 'WEB', '3', '1', '2', '', '', '','房易融','房易融'),
                            ('web_projectCategory_all', 'WEB', '', '1', '8', '', '', '', '车易融','车易融'),
-                           ('web_page', 'WEB', '', '1', '2', '', '', '', '车易融', '信易融'),
-                           ('web_page', 'WEB', '', '2', '2', '', '', '', '信易融', '信易融'),
+                           # ('web_page', 'WEB', '', '1', '2', '', '', '', '车易融', '信易融'),
+                           # ('web_page', 'WEB', '', '2', '2', '', '', '', '信易融', '信易融'),
                            ])
 
-    def test_projectls(self, name, source, projectCategory, page, pageSize,
+    def test_projectCategory(self, name, source, projectCategory, page, pageSize,
                       projectStatus, searchTime, projectNewType, message1,message2):
+        """WEB项目类型查询测试"""
         stampToken = str(get_stampToken())
         test_data1 = {'stampToken': stampToken,'source': source,
                      'page': page, 'pageSize': pageSize, 'searchTime': searchTime,
@@ -40,6 +41,28 @@ class ProjectList(unittest.TestCase):
         self.assertEqual(result['item']['list'][0]['projectCategory'], message1)
         self.assertEqual(result['item']['list'][-1]['projectCategory'], message2)
 
+    @parameterized.expand([('web_pagesize_8', 'WEB', '4', '1', '8', '', '', '','8'),
+                           ('web_pagesize_4', 'WEB', '4', '1', '4', '', '', '', '4'),
+                           ('APP_pagesize_8', 'APP', '4', '1', '8', '', '', '', '8'),
+                           ('APP_pagesize_4', 'APP', '4', '1', '4', '', '', '', '4'),])
+    def test_pagesize(self, name, source, projectCategory, page, pageSize,
+                      projectStatus, searchTime, projectNewType, num):
+        """WEB、APP每页条数测试"""
+        stampToken = str(get_stampToken())
+        test_data1 = {'stampToken': stampToken, 'source': source,
+                      'page': page, 'pageSize': pageSize, 'searchTime': searchTime,
+                      'projectNewType': projectNewType, 'projectCategory': projectCategory}
+        checkToken = get_chekToken(**test_data1)
+        test_data = {'stampToken': stampToken, 'checkToken': checkToken, 'source': source,
+                     'page': page, 'pageSize': pageSize, 'searchTime': searchTime,
+                     'projectNewType': projectNewType, 'projectCategory': projectCategory,
+                     'projectStatus': projectStatus}
+
+        r = requests.request('get', url=self.base_url, params=test_data)
+        result = r.json()
+        self.assertEqual(len(result['item']['list']),int(num))
+
+
     @parameterized.expand([('web_inprocess', 'WEB', '', '1', '8', '1', '', '', 'inProcess', 'inProcess', 'status'),
                            ('web_finish', 'WEB', '', '1', '8', '2', '', '', 'finish','finish','status'),
                            ('web_clear', 'WEB', '', '1', '8', '3', '', '', 'clear', 'clear', 'status'),
@@ -50,12 +73,13 @@ class ProjectList(unittest.TestCase):
                            ('web_time_3-6', 'WEB', '', '1', '8', '0', '3', '', '4.0', '3.0', 'financingMaturity'),
                            ('web_time_6-12', 'WEB', '', '1', '8', '0', '4', '', '6.0', '7.0', 'financingMaturity'),
                            ('web_time_12-24', 'WEB', '', '1', '8', '0', '4', '', '12.0', '24.0', 'financingMaturity'),
-                           ('web_group_search1', 'WEB', '2', '1', '8', '1', '4', '', '信1', '信2', 'projectName'),
-                          ('web_group_search2', 'WEB', '1', '1', '8', '3', '3', '', '车1', '车1', 'projectName'),
-                           ('web_group_search3', 'WEB', '4', '1', '8', '2', '2', '', '企2', '企2', 'projectName'),
+                          #  ('web_group_search1', 'WEB', '2', '1', '8', '1', '4', '', '信1', '信2', 'projectName'),
+                          # ('web_group_search2', 'WEB', '1', '1', '8', '3', '3', '', '车1', '车1', 'projectName'),
+                          #  ('web_group_search3', 'WEB', '4', '1', '8', '2', '2', '', '企2', '企2', 'projectName'),
                            ])
-    def test_projectls_status(self, name, source, projectCategory, page, pageSize,
+    def test_status_and_searchTime(self, name, source, projectCategory, page, pageSize,
                               projectStatus, searchTime, projectNewType, first_value, last_value, key):
+        """WEB项目状态与借款期限测试"""
         stampToken = str(get_stampToken())
         test_data1 = {'stampToken': stampToken, 'source': source,
                       'page': page, 'pageSize': pageSize, 'searchTime': searchTime,
@@ -74,11 +98,13 @@ class ProjectList(unittest.TestCase):
         self.assertEqual(result['item']['list'][0][key], first_value)
         self.assertEqual(result['item']['list'][-1][key], last_value)
 
-    @parameterized.expand([('web_pagesize10', 'WEB', '', '1', '4', '', '', '','4'),
-                           ('web_pagesize10', 'WEB', '', '1', '8', '', '', '', '8'),
+    @parameterized.expand([('web_group_search1', 'WEB', '2', '1', '8', '1', '4', '', '信1', '信2', 'projectName'),
+                           ('web_group_search2', 'WEB', '1', '1', '8', '3', '3', '', '车1', '车1', 'projectName'),
+                           ('web_group_search3', 'WEB', '4', '1', '8', '2', '2', '', '企2', '企2', 'projectName'),
                            ])
-    def test_projectls_pagesize(self, name, source, projectCategory, page, pageSize,
-                              projectStatus, searchTime, projectNewType,assert_text):
+    def test_group_terms(self, name, source, projectCategory, page, pageSize,
+                              projectStatus, searchTime, projectNewType,first_value, last_value, key):
+        """WEB多条件组合查询测试"""
         stampToken = str(get_stampToken())
         test_data1 = {'stampToken': stampToken, 'source': source,
                       'page': page, 'pageSize': pageSize, 'searchTime': searchTime,
@@ -94,7 +120,8 @@ class ProjectList(unittest.TestCase):
         result = r.json()
         print(result)
         self.assertEqual(result['code'], 1)
-        self.assertEqual(len(result['item']['list']), int(assert_text))
+        self.assertEqual(result['item']['list'][0][key], first_value)
+        self.assertEqual(result['item']['list'][-1][key], last_value)
 
 
 

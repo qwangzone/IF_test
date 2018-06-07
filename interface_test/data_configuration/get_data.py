d276a5d0bc4412d7bf0c61e74e1311fb12de4c91
wangsumei1992 <w41142519931028>
2018-06-07 15:32:31 +0800

    首页test

--- interface_test/data_configuration/get_data.py
+++ interface_test/data_configuration/get_data.py
@@ -4,4 +4,4 @@ class GetData:
     file_path = dir + "/url_data.ini"
     cf = configparser.ConfigParser()
     cf.read(file_path, encoding='utf-8')
-    url = cf.get("url","test_url")
+    url = cf.get("url", "test_url")
